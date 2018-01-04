5d189aec14b0ec68a39f195192ee14ec875f9acf
Max Donchenko <wereya2@yandex.ru>
2018-01-04 20:45:45 +0200

    Revert "downgrade to ES5 syntax"
    This reverts commit 212470d

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const {
 } = require('./helpers.js');
 
 module.exports = function multiply(first, second) {
-    var productSums = [];
-    for (var i = second.length - 1, j = 0; i >= 0; i--, j++) {
-        var localProduct = multiplyByDigit(first, second[i]) + '0'.repeat(j);
-        productSums.push(localProduct);
-    }
-    return addByDigits(productSums);
+  let productSums = [];
+  for (
+      let i = second.length - 1, j = 0;
+      i >= 0;
+      i--, j++
+  ) {
+    const localProduct = multiplyByDigit(first, second[i]) + '0'.repeat(j);
+    productSums.push(localProduct);
+  }
+  return addByDigits(productSums);
 };
