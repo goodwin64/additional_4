212470de96cce2b1d6e2ad4125fcfa512d38fdcf
Max Donchenko <wereya2@yandex.ru>
2018-01-04 20:40:05 +0200

    downgrade to ES5 syntax

--- src/helpers.js
+++ src/helpers.js
@@ -4,41 +4,43 @@ module.exports = {
 };
 
 function multiplyByDigit(number, digit) {
-    let surplus = 0;
-    const resultWithoutLastSurplus = String(number)
-        .split('')
-        .reduceRight((acc, currDigit) => {
-            const currentProduct = digit * currDigit + Number(surplus);
-            const remainder = currentProduct % 10;
-            surplus = (currentProduct - remainder) / 10;
-            return `${remainder}${acc}`;
-        }, '');
-    const resultWithLastSurplus = `${nonZeroToString(surplus)}${resultWithoutLastSurplus}`;
-    return resultWithLastSurplus === ''
-        ? '0'
-        : resultWithLastSurplus;
+    var surplus = 0;
+    var resultWithoutLastSurplus = String(number).split('').reduceRight(function (acc, currDigit) {
+        var currentProduct = digit * currDigit + Number(surplus);
+        var remainder = currentProduct % 10;
+        surplus = (currentProduct - remainder) / 10;
+        return '' + remainder + acc;
+    }, '');
+    var resultWithLastSurplus = '' + nonZeroToString(surplus) + resultWithoutLastSurplus;
+    return resultWithLastSurplus === '' ? '0' : resultWithLastSurplus;
 }
 
 function nonZeroToString(number) {
-    return `${Number(number) !== 0 ? number : ''}`;
+    return '' + (Number(number) !== 0 ? number : '');
 }
 
 function addByDigits(numbers) {
-    const maxLength = Math.max.apply(null, numbers.map(str => str.length));
-    const notations = numbers.map(number => {
-        const zerosPrefix = '0'.repeat(maxLength - number.length);
+    var maxLength = Math.max.apply(null, numbers.map(function (str) {
+        return str.length;
+    }));
+    var notations = numbers.map(function (number) {
+        var zerosPrefix = '0'.repeat(maxLength - number.length);
         return zerosPrefix + number;
     });
-    let result = '';
-    let surplus = 0;
-    for (let i = maxLength - 1; i >= 0; i--) {
-        const currDigitSum = notations
-            .reduce((acc, curr) => {
-                return acc + Number(curr[i]);
-            }, 0) + surplus;
-        const remainder = currDigitSum % 10;
+    var result = '';
+    var surplus = 0;
+
+    var _loop = function _loop(i) {
+        var currDigitSum = notations.reduce(function (acc, curr) {
+            return acc + Number(curr[i]);
+        }, 0) + surplus;
+        var remainder = currDigitSum % 10;
         surplus = (currDigitSum - remainder) / 10;
         result = remainder + result;
+    };
+
+    for (var i = maxLength - 1; i >= 0; i--) {
+        _loop(i);
     }
     return nonZeroToString(surplus) + result;
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ const {
 } = require('./helpers.js');
 
 module.exports = function multiply(first, second) {
-  let productSums = [];
-  for (
-      let i = second.length - 1, j = 0;
-      i >= 0;
-      i--, j++
-  ) {
-    const localProduct = multiplyByDigit(first, second[i]) + '0'.repeat(j);
-    productSums.push(localProduct);
-  }
-  return addByDigits(productSums);
+    var productSums = [];
+    for (var i = second.length - 1, j = 0; i >= 0; i--, j++) {
+        var localProduct = multiplyByDigit(first, second[i]) + '0'.repeat(j);
+        productSums.push(localProduct);
+    }
+    return addByDigits(productSums);
 };
